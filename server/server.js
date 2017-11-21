3d0a2f67c1e569215d010af583030d60251842d7
unknown <cattuong.cloud@gmail.com>
2017-11-21 11:32:31 +0700

    commit update function

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 var app = express();
 
 app.use(bodyParser.json());
@@ -59,10 +60,37 @@ app.get('/todos', (req, res) => {
         return res.status(404).send();
       }
   
-      res.send(todo);
+      res.send({todo});
     }).catch((e) => {
       res.status(400).send();
     });
+  });
+
+
+  app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+  
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+  
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+  
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+  
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    })
   });
 
   app.listen(port, () => {
