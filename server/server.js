c24bcda980c103fe8afe36fd68683886ec7c9ae6
unknown <cattuong.cloud@gmail.com>
2017-11-25 16:56:59 +0700

    test post user

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 const port = process.env.PORT;
 var app = express();
 
@@ -92,6 +93,24 @@ app.get('/todos', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
+// user API
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
   app.listen(port, () => {
